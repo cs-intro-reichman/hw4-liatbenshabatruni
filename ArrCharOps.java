93052770f2ba2c370ae1d71ea5fe7f4e12129420
liatbenshabatruni <liat.benshabat@post.runi.ac.il>
2024-11-29 18:51:58 +0200

    question 1 for the last time amen

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -176,6 +176,9 @@ public class ArrCharOps {
     if (str1.equals(str2)){
         return 0;
     }
+    if (str1.isEmpty()|| str2.isEmpty()){
+        return -2;
+    }
     int minL = Math.min((str1.length()), str2.length());
     for (int i=0;i<minL;i++){
         if (str1.charAt(i)<str2.charAt(i)){
@@ -186,7 +189,7 @@ public class ArrCharOps {
         }
     }
     if(str1.length()!=str2.length()){
-        if(str1.length()==minL){
+        if(str1.length()<str2.length()){
             return -1;
         }
         else{
