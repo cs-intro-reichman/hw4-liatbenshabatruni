3fd2abac0b2fbd36b76b9703a4e2306894df172f
liatbenshabatruni <liat.benshabat@post.runi.ac.il>
2024-11-30 01:52:51 +0200

    q2

--- MyString.java
+++ MyString.java
@@ -6,25 +6,53 @@ public class MyString {
         System.out.println("TLV : " + lowerCase("TLV"));
         System.out.println("lowercase : " + lowerCase("lowercase"));
 
-        System.out.println("Testing contains:");
+        /*System.out.println("Testing contains:");
         System.out.println(contains("unhappy", "happy")); // true
         System.out.println(contains("happy", "unhappy")); // false
         System.out.println(contains("historical", "story")); // false
         System.out.println(contains("psychology", "psycho")); // true
         System.out.println(contains("personality", "son")); // true
         System.out.println(contains("personality", "dad")); // false
-        System.out.println(contains("resignation", "sign")); // true
+        System.out.println(contains("resignation", "sign")); // true*/
     }
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        
+        String a= "";
+        for (int i = 0; i < str.length(); i++) {
+            if ((str.charAt(i)<=90)&&(str.charAt(i)>=65)){
+                a += (char)(str.charAt(i)+32);
+            } 
+            else {
+                a += str.charAt(i);
+            } 
+        }
+        return a;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        int i = 0;
+        int j = 0;
+        if (str1.equals(str2)){
+            return true;
+        }
+        if (str1.length()>str2.length()){
+            while (i < str1.length()) {
+                    if (str1.charAt(i)==str2.charAt(j)){
+                        i++;
+                        j++;
+                        if (str2.length()==j){
+                            return true;
+                    }
+                    }
+                    else {
+                        i++;
+                        j=0;
+                    }
+                }
+            }
         return false;
     }
 }
